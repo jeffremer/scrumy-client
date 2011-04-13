9b0b29208373ead5d41e4327fabc2351a6acc192
Jeff Remer <jeff@threestarchina.com>
2011-04-12 21:33:08 -0700

    Added module, initial classes

--- scrumy.rb
+++ scrumy.rb
@@ -2,64 +2,84 @@ require 'json'
 require 'rest_client'
 require 'fastercsv'
 
-class ScrumyClient
-  attr_reader :url
-  def initialize(project, password)
-    @project, @password = project, password
-  end
-
-  def sprint(id="current")
-    if id == "current"
-      @url = "https://#{@project}:#{@password}@scrumy.com/api/scrumies/#{@project}/sprints/#{id}.json"
-    else
-      @url = "https://#{@project}:#{@password}@scrumy.com/api/sprints/#{id}.json"
+module Scrumy
+  class Client
+    attr_reader :url
+    def initialize(project, password)
+      @project, @password = project, password
     end
-    get(@url, 'sprint')
-  end
-  
-  def sprints
-    @url = "https://#{@project}:#{@password}@scrumy.com/api/scrumies/#{@project}/sprints.json"
-    get(url, nil)
-  end
 
-  def snapshots(id="current")
-    sprint_id = sprint(id)['id']
-    @url = "https://#{@project}:#{@password}@scrumy.com/api/sprints/#{sprint_id}/snapshots.json"
-    get(@url, nil)
-  end
+    def sprint(id="current")
+      if id == "current"
+        @url = "https://#{@project}:#{@password}@scrumy.com/api/scrumies/#{@project}/sprints/#{id}.json"
+      else
+        @url = "https://#{@project}:#{@password}@scrumy.com/api/sprints/#{id}.json"
+      end
+      Sprint.new(get(@url, 'sprint'))
+    end
   
-  def regression_steps(include_header=true)
-      current_sprint = sprint
-      rows = current_sprint['stories'].collect{|story|
-        if !story['story']['tasks'].nil?
-          [story['story']['title']].push(
-            story['story']['tasks'].collect{|task|
-              task['task']['scrumer']['name']
-            }.uniq().reject{|n| n == 'info'}.join(', ')
-          )
-        else
-          [story['story']['title']]
-        end
-      }
-      rows.unshift(['Scrumy Item'.upcase, 'Worked On'.upcase]) if include_header && !rows.nil?
-      rows
-  end
+    def sprints
+      @url = "https://#{@project}:#{@password}@scrumy.com/api/scrumies/#{@project}/sprints.json"
+      get(url, nil)
+    end
+
+    def snapshots(id="current")
+      sprint_id = sprint(id)['id']
+      @url = "https://#{@project}:#{@password}@scrumy.com/api/sprints/#{sprint_id}/snapshots.json"
+      get(@url, nil)
+    end
   
-  protected
+    def regression_steps(include_header=true)
+        current_sprint = sprint
+        rows = current_sprint['stories'].collect{|story|
+          if !story['story']['tasks'].nil?
+            [story['story']['title']].push(
+              story['story']['tasks'].collect{|task|
+                task['task']['scrumer']['name']
+              }.uniq().reject{|n| n == 'info'}.join(', ')
+            )
+          else
+            [story['story']['title']]
+          end
+        }
+        rows.unshift(['Scrumy Item'.upcase, 'Worked On'.upcase]) if include_header && !rows.nil?
+        rows
+    end
+  
+    protected
     
-    def get(url, root)
-      RestClient.get(url){|response, request, result, &block|
-        case response.code
-        when 200
-          json = JSON.parse(response.to_str)
-          root ? json[root] : json
-        else
-          response.return!(request, result, &block)
-        end
-      }
-    end
+      def get(url, root)
+        RestClient.get(url){|response, request, result, &block|
+          case response.code
+          when 200
+            json = JSON.parse(response.to_str)
+            root ? json[root] : json
+          else
+            response.return!(request, result, &block)
+          end
+        }
+      end
   
-end
+  end
+  
+  class Object
+    attr_reader :id
+    def initialize args
+      args.each do |k,v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+    end
+  end  
+  class Sprint
+    attr_reader :start_date
+  end
+  class Story
+  end
+  class Task
+  end
+  class Scrumer
+  end
+end  
 
 class Array
   def to_csv
