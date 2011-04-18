fd9015e25a027266491493c9050486f743a69173
Jeff Remer <jeff@threestarchina.com>
2011-04-18 12:35:26 -0700

    Added comments for rocco and added spec for fetching a sprint by ID

--- spec/scrumy_client_spec.rb
+++ spec/scrumy_client_spec.rb
@@ -11,14 +11,22 @@ describe Scrumy::Client do
     it "fetches a full current sprint" do
       sprint = @client.sprint
       sprint.stories.size.should > 0
-      sprint.client.class.should == Scrumy::Client       
+      sprint.should be_an_instance_of Scrumy::Sprint
+    end
+  end
+
+  describe '#sprint(id)' do
+    it "fetches a sprint by ID" do
+      sprints = @client.sprints
+      sprint = @client.sprint(sprints[1].id)
+      sprint.should be_an_instance_of Scrumy::Sprint
     end
   end
   
   describe '#sprints' do
     it "fetches a list of sprints" do
       @client.sprints.each do |sprint|
-        sprint.class.should == Scrumy::Sprint
+        sprint.should be_an_instance_of Scrumy::Sprint
       end
     end
   end
@@ -27,7 +35,7 @@ describe Scrumy::Client do
     it "fetches a list of stories based on a sprint" do
       sprints = @client.sprints
       sprints[1].stories.each do |story|
-        story.class.should == Scrumy::Story
+        story.should be_an_instance_of Scrumy::Story
       end
     end
   end
@@ -35,8 +43,8 @@ describe Scrumy::Client do
   describe '#tasks' do
     it "fetches a list of tasks based on a story" do
       @client.sprints[1].stories[1].tasks do |task|
-        task.class.should.kind_of? Scrumy::Task
-        task.scrumer.class.should == Scrumy::Scrumer
+        task.should be_an_instance_of Scrumy::Task
+        task.scrumer.should be_an_instance_of Scrumy::Scrumer
         puts task.scrumer
       end
     end
@@ -45,7 +53,7 @@ describe Scrumy::Client do
   describe '#scrumers' do
     it "fetches a list of scrumers by scrumy" do
       @client.scrumers.each do |scrumer|
-        scrumer.class.should == Scrumy::Scrumer
+        scrumer.should be_an_instance_of Scrumy::Scrumer
       end
     end
   end
